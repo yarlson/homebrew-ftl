930aa407b67191346d6f48cd641a82f315077df0
Yar Kravtsov <yarlson@gmail.com>
2025-01-12 10:02:48 +0200

    Brew formula update for ftl version 0.22.2

--- ftl.rb
+++ ftl.rb
@@ -5,21 +5,21 @@
 class Ftl < Formula
   desc "FTL - Faster Than Light deployment tool"
   homepage "https://github.com/yarlson/ftl"
-  version "0.22.1"
+  version "0.22.2"
   license "MIT"
 
   on_macos do
     on_intel do
-      url "https://github.com/yarlson/ftl/releases/download/0.22.1/ftl_0.22.1_darwin_amd64.tar.gz"
-      sha256 "cc1c98706a1cdc816dc31d4d2b43cd712abaf42b80a2151547876394662438fb"
+      url "https://github.com/yarlson/ftl/releases/download/0.22.2/ftl_0.22.2_darwin_amd64.tar.gz"
+      sha256 "a630c47eeff9cbea10d385bbbd6e3c3c360ba1a01fda854828cf7be31f1a3215"
 
       def install
         bin.install "ftl"
       end
     end
     on_arm do
-      url "https://github.com/yarlson/ftl/releases/download/0.22.1/ftl_0.22.1_darwin_arm64.tar.gz"
-      sha256 "085a90d7a178a82043d33d0b81c82d119452aea8bcc01164314d5e5811c02046"
+      url "https://github.com/yarlson/ftl/releases/download/0.22.2/ftl_0.22.2_darwin_arm64.tar.gz"
+      sha256 "705eed1d9261d4be8d72427a1622d00fe39c86885c5038f3c2c5469795cb7199"
 
       def install
         bin.install "ftl"
@@ -30,8 +30,8 @@ class Ftl < Formula
   on_linux do
     on_intel do
       if Hardware::CPU.is_64_bit?
-        url "https://github.com/yarlson/ftl/releases/download/0.22.1/ftl_0.22.1_linux_amd64.tar.gz"
-        sha256 "294946370b58543405ed97c240fefe240e64aab99e7cc2129b42fbdc80895fb6"
+        url "https://github.com/yarlson/ftl/releases/download/0.22.2/ftl_0.22.2_linux_amd64.tar.gz"
+        sha256 "a80771df11cd7519de3ae72fba63ebdc4082d9a997a2b3aa7f67ab82998af270"
 
         def install
           bin.install "ftl"
@@ -40,8 +40,8 @@ class Ftl < Formula
     end
     on_arm do
       if Hardware::CPU.is_64_bit?
-        url "https://github.com/yarlson/ftl/releases/download/0.22.1/ftl_0.22.1_linux_arm64.tar.gz"
-        sha256 "8861b3caa3938b6e7ec5c77816d76b7860f8b41152355281b17ea089321604a7"
+        url "https://github.com/yarlson/ftl/releases/download/0.22.2/ftl_0.22.2_linux_arm64.tar.gz"
+        sha256 "8e2e1c1e592ae4246a8c5da6102e09fb4fac15280d05fa031eadd73bfaf69870"
 
         def install
           bin.install "ftl"
